b0c44d37ece1a7625c381865da8539633db8e5a5
igormoreiraalves <igor@biomainvestimentos.com>
2024-04-23 19:26:56 -0300

    CHORE: added HashUtil

---
+++ src/main/java/com/luiza/labs/domain/HashUtil.java
@@ -0,0 +1,25 @@
+package com.luiza.labs.domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtil {
+
+    public static String calculateHash(MultipartFile file) throws IOException, NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hashBytes = digest.digest(file.getBytes());
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte hashByte : hashBytes) {
+            String hex = Integer.toHexString(0xff & hashByte);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
