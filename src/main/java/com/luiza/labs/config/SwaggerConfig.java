588ddfe4a31c5f1b17270bc2e5a757be010dcd5c
igormoreiraalves <igor@biomainvestimentos.com>
2024-04-24 21:41:39 -0300

    CHORE: refactored SwaggerConfig

--- src/main/java/com/luiza/labs/config/SwaggerConfig.java
+++ src/main/java/com/luiza/labs/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.luiza.labs.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    @ConditionalOnMissingBean
-    public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new io.swagger.v3.oas.models.OpenAPI()
                 .components(new Components())
                 .info(new Info().title("File transformer").description("Serviço que processa um arquivo de pedidos desnormalizado").version("0.0.1"));
