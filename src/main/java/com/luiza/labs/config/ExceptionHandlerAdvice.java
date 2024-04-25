ce12146d9005336ed98669c670a9900c6f0b1a95
igormoreiraalves <igor@biomainvestimentos.com>
2024-04-25 15:07:53 -0300

    CHORE: added log

--- src/main/java/com/luiza/labs/config/ExceptionHandlerAdvice.java
+++ src/main/java/com/luiza/labs/config/ExceptionHandlerAdvice.java
@@ -1,6 +1,8 @@
 package com.luiza.labs.config;
 
 import com.luiza.labs.domain.exception.LabsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -13,8 +15,11 @@ import java.net.URI;
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
+    private final Logger log = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
+
     @ExceptionHandler({LabsException.class})
     public ResponseEntity<ProblemDetail> handlerLabsException(LabsException exception) {
+        log.error(exception.getMessage());
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
         problemDetail.setType(URI.create("application.rules"));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
@@ -22,6 +27,7 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(TypeMismatchException.class)
     public ResponseEntity<ProblemDetail> handlerTypeMismatchException(TypeMismatchException exception) {
+        log.error(exception.getMessage());
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
         problemDetail.setType(URI.create("argument.invalid"));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
@@ -29,6 +35,7 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetail> handlerException(Exception exception) {
+        log.error(exception.getMessage(), exception.getCause());
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
         problemDetail.setType(URI.create("internal.error"));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetail);
