304b105bd8cd4b165eb2949b7cb8a7751b2fc110
igormoreiraalves <igor@biomainvestimentos.com>
2024-04-24 21:55:41 -0300

    FIX: tests

--- src/test/java/com/luiza/labs/domain/InputLineTest.java
+++ src/test/java/com/luiza/labs/domain/InputLineTest.java
@@ -1,6 +1,5 @@
-package com.luiza.labs.infrastructure;
+package com.luiza.labs.domain;
 
-import com.luiza.labs.domain.InputLine;
 import com.luiza.labs.domain.exception.InputException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,14 +13,14 @@ class InputLineTest {
     @Test
     void shouldParseDataSuccessfully() throws InputException {
         String input = "0000000070                              Palmer Prosacco00000007530000000003     1836.7420210308\n";
-        InputLine inputLine = InputLine.parser(input);
+        Item item = InputLine.parser(input);
 
-        Assertions.assertEquals(70L, inputLine.getUserId());
-        Assertions.assertEquals("Palmer Prosacco", inputLine.getName());
-        Assertions.assertEquals(753L, inputLine.getOrderId());
-        Assertions.assertEquals(3L, inputLine.getProductId());
-        Assertions.assertEquals(BigDecimal.valueOf(1836.74), inputLine.getProductPrice());
-        Assertions.assertEquals(LocalDate.parse("2021-03-08"), inputLine.getOrderDate());
+        Assertions.assertEquals(70L, item.getUserId());
+        Assertions.assertEquals("Palmer Prosacco", item.getUserName());
+        Assertions.assertEquals(753L, item.getOrderId());
+        Assertions.assertEquals(3L, item.getProductId());
+        Assertions.assertEquals(BigDecimal.valueOf(1836.74), item.getProductValue());
+        Assertions.assertEquals(LocalDate.parse("2021-03-08"), item.getOrderDate());
     }
 
     @Test
